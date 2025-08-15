557d678b08eb0131422c853a6359ad5f347a6d60
Andrew <93899022+Galahad47@users.noreply.github.com>
2025-08-15 12:43:35 +0300

    Добавил проверку докера, инета, гита

--- #!/bin/test.sh
+++ #!/bin/test.sh
@@ -1,8 +1,22 @@
 #!/bin/test
-
-if ! docker info >/dev/null 2>&1; 
-then echo "Error {TypeERROR}"
-exit 1
+#докер
+if ! docker info >/dev/null 2>&1; then 
+    echo "Error {TypeERROR}"
 fi
 
-if ! ping 8.8.8.8 >/dev/ping_null;
+echo -e "GET http://google.com HTTPS/1.0\n\n" | google.com 80 > dev/null 2>&1
+#инет
+if [$? -eq 0]; then
+    echo 'Online'
+else
+    echo 'Offline'
+#гит
+if timeout 30s git ls-remote --tags > /dev/null 2>&1; then
+    echo "git-sever доступен"
+else 
+    echo "git-server недоступен"
+fi
+#создание файла докера
+docker build 
+
+

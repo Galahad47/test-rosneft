7f9812a1134c46d705101e425ec9c24afdad04a6
Andrew <93899022+Galahad47@users.noreply.github.com>
2025-08-18 17:08:57 +0300

    Запутался с докером. Разберусь - загружу

--- #!/bin/test.sh
+++ #!/bin/test.sh
@@ -6,25 +6,26 @@ if ! systemctl is-active --quiet docker; then
     exit 1
 fi
 if ! ping -c 4 -q 8.8.8.8; then
-        echo "Нет сети"
+        echo -e "\e[0;31mНет сети"
 else
-        echo "Подключение к сети есть"
+        echo -e "\e[1;32mПодключение к сети есть"
 fi
-if timeout 30s git ls-remote https://github.com/Lissy93/dashy > /dev/null 2>&1; then
-        echo "Репозиторий доступен"
+if timeout 30s git ls-remote https://github.com/Lissy93/dashy.git > /dev/null 2>&1; then
+        echo -e "\e[0;32mРепозиторий доступен"
 else
-        echo "Доступ к репозиторию ограничен"
+        echo -e "\e[1;31mДоступ к репозиторию ограничен"
         exit 1
 fi
 
+clone https://github.com/Lissy93/dashy.git
 mkdir -p ~/dashy && cd ~/dashy && curl -o config.yml https://raw.githubusercontent.com/Lissy93/dashy/main/config.yml
-docker run  -d --name dashy -p 8080:80 -v ~/dashy/config.yml:/app/config.yml lissy93/dashy
-timeout 10s
-docker save -o test_rosneft.tar dashy
-timeout 5s
+docker run  -d --name dashy -p 8080:80 -v ~/dashy/config.yml:/app/config.yml lissy93/dashy.git
+docker save -o dashy.tar dashy
+
 docker stop $(docker ps -qa) && docker rm $(docker ps -qa)
-timeout 10s
-docker load -i test_rosneft.tar --build && docker images
+
+docker load -i dashy.tar && docker images | grep dashy
+
 
 cat > docker-compose.yml <<EOF
 version: '3'
@@ -36,5 +37,5 @@ services:
         restart: unleess-stopped
 EOF
 
-docker-compose up -d --build && curl -I http://localhost:8080
+docker-compose up -d && curl -I http://localhost:8080
 
